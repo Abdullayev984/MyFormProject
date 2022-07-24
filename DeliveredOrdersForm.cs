28cf2670904e84d808b5067131849dd6d1ef11fa
Abdullayev984 <56773370+Abdullayev984@users.noreply.github.com>
2022-07-24 19:39:36 +0400

    Add files via upload

---
+++ DeliveredOrdersForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CargoProgram;
+
+namespace CargoProqram
+{
+    public partial class DeliveredOrdersForm : Form
+    {
+        public DeliveredOrdersForm()
+        {
+            InitializeComponent();
+        }
+        SqlConnection myConnection = new SqlConnection("Data Source=DESKTOP-1T63NNN;Initial Catalog=Orders;Integrated Security=true");
+        private void ShowData(string data)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(data, myConnection);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+        private void DeliveredOrdersForm_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+            ShowData("select * from DeliveredOrders");
+            dataGridView1.AllowUserToAddRows = false;
+        }
+  
+
+        private void GoNextForm_Click(object sender, EventArgs e)
+        {
+            OrderForm fr = new OrderForm();
+            this.Hide();
+            fr.Show();
+        }
+
+       
+
+        private void AddOrder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myConnection.Open();
+                SqlCommand cmd = new SqlCommand("insert into DeliveredOrders(NameSurname,StockCode,DeliveryDate,AmountPaid) values (@NameSurname,@StockCode,@DeliveryDate,@AmountPaid)", myConnection);
+
+                cmd.Parameters.AddWithValue("@NameSurname", NameSurname.Text);
+                cmd.Parameters.AddWithValue("@StockCode", StockCode.Text);
+                cmd.Parameters.AddWithValue("@DeliveryDate", DeliveryDate.Text);
+                cmd.Parameters.AddWithValue("@AmountPaid", Convert.ToDouble(AmountPaid.Text));
+                if (StockCode.Text == "")
+                {
+                    MessageBox.Show("Enter Stock Code:");
+                    NameSurname.Clear();
+                    StockCode.Clear();
+                    AmountPaid.Clear();
+                }
+                if (StockCode.Text != "")
+                {
+                    cmd.ExecuteNonQuery();
+
+                }
+
+                ShowData("select * from DeliveredOrders");
+                NameSurname.Clear();
+                StockCode.Clear();
+                DeliveryDate.Clear();
+                AmountPaid.Clear();
+
+                myConnection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void DeleteForDate_Click(object sender, EventArgs e)
+        {
+            myConnection.Open();
+            SqlCommand cmd = new SqlCommand("delete from DeliveredOrders where DeliveryDate=@DeliveryDate", myConnection);
+            cmd.Parameters.AddWithValue("@DeliveryDate", DeletedForDate.Text.Trim());
+            if (DeletedForDate.Text.Trim() != "")
+            {
+                cmd.ExecuteNonQuery();
+            }
+            ShowData("select * from DeliveredOrders");
+            DeletedForDate.Clear();
+            myConnection.Close();
+        }
+
+        private void DeleteForStock_Click(object sender, EventArgs e)
+        {
+            myConnection.Open();
+            SqlCommand cmd = new SqlCommand("delete from DeliveredOrders where StockCode=@StockCode", myConnection);
+            cmd.Parameters.AddWithValue("@StockCode", DeletedStockCode.Text);
+            if (DeletedStockCode.Text.Trim() != "")
+            {
+
+                cmd.ExecuteNonQuery();
+            }
+            ShowData("select * from DeliveredOrders");
+            DeletedStockCode.Clear();
+            myConnection.Close();
+        }
+
+        private void FinalAmount_Click(object sender, EventArgs e)
+        {
+            double cem = 0;
+
+            int b = dataGridView1.RowCount;
+            for (int i = 0; i < b; i++)
+            {
+
+                cem = cem + Convert.ToDouble(dataGridView1.Rows[i].Cells[3].Value.ToString());
+
+
+            }
+            Final_Amount.Text = cem.ToString() + " AZN";
+        }
+    }
+}
---
+++ OrderForm.cs
@@ -0,0 +1,114 @@
+﻿using CargoProqram;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CargoProgram
+{
+    public partial class OrderForm : Form
+    {
+        public OrderForm()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection myConnection = new SqlConnection("Data Source=DESKTOP-1T63NNN;Initial Catalog=Orders;Integrated Security=true");
+        private void ShowData(string data)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(data, myConnection);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+        private void OrderForm_Load(object sender, EventArgs e)
+        {
+          
+            ShowData("select * from Order");
+            dataGridView1.Columns["Orderİd"].Visible = false;
+            dataGridView1.AllowUserToAddRows = false;
+
+        }
+
+
+
+        private void AddOrder_Click(object sender, EventArgs e)
+        {
+        try
+        { 
+                double a = Convert.ToDouble(PurchasePrice.Text);
+                double b = Convert.ToDouble(SalePrice.Text);
+                double d = Convert.ToDouble(PaymentStatus.Text);
+                double c = b - d;
+                myConnection.Open();
+                string sql = @"insert into Order(NameSurname,ProductName,StockCode,Link,Size,DateofOrder,Firm,PurchasePrice,SalePrice,PaymentStatus,QalıqBorc,OrderStatus,PlaceofDelivery,Note,PhoneNumber) values (@NameSurname,@ProductName,@StockCode,@Link,@Size,@DateofOrder,@Firm,@PurchasePrice,@SalePrice,@PaymentStatus,@OrderStatus,@PlaceofDelivery,@Note,@PhoneNumber)";
+                SqlCommand cmd = new SqlCommand(sql, myConnection);
+
+                cmd.Parameters.AddWithValue("@NameSurname", NameSurname.Text);
+                cmd.Parameters.AddWithValue("@ProductName", ProductName.Text);
+               cmd.Parameters.AddWithValue("@StockCode", StockCode.Text);
+                if (StockCode.Text == "")
+                {
+                    MessageBox.Show("Enter Stock Code:");
+
+                }
+                cmd.Parameters.AddWithValue("@Link", Link.Text);
+                cmd.Parameters.AddWithValue("@Size", Size.Text);
+                cmd.Parameters.AddWithValue("@DateofOrder", Date.Text);
+                cmd.Parameters.AddWithValue("@Firm", FirmName.Text);
+                cmd.Parameters.AddWithValue("@PurchasePrice", a);
+                cmd.Parameters.AddWithValue("@@SalePrice", Convert.ToDouble(SalePrice.Text));
+                cmd.Parameters.AddWithValue("@PaymentStatus", Convert.ToDouble(PaymentStatus.Text));
+                cmd.Parameters.AddWithValue("@QalıqBorcuu", c);
+                cmd.Parameters.AddWithValue("@OrderStatus", OrderStatus.Text);
+                cmd.Parameters.AddWithValue("PlaceofDeliver", PlaceofDelivery.Text);
+                cmd.Parameters.AddWithValue("@Note", Note.Text);
+                cmd.Parameters.AddWithValue("@PhoneNumber", PhoneNumber.Text);
+                if (StockCode.Text != "")
+                {
+                    cmd.ExecuteNonQuery();
+
+                }
+
+            ShowData("select * from Order");
+                NameSurname.Clear();
+                ProductName.Clear();
+                StockCode.Clear();
+                Link.Clear();
+                Size.Clear();
+                FirmName.Clear();
+                PurchasePrice.Clear();
+                SalePrice.Clear();
+                PaymentStatus.Clear();
+                PlaceofDelivery.Clear();
+                Note.Clear();
+                PhoneNumber.Clear();
+                OrderStatus.Text = "";
+                myConnection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void Delete_Order_Click(object sender, EventArgs e)
+        {
+            myConnection.Open();
+            SqlCommand cmd = new SqlCommand("delete from Order where StockCode=@StockCode", myConnection);
+            cmd.Parameters.AddWithValue("@StockCode", Stock_Code.Text);
+            if (Stock_Code.Text.Trim() != "")
+            {
+                cmd.ExecuteNonQuery();
+            }
+            ShowData("select * from Order ");
+            Stock_Code.Clear();
+            myConnection.Close();
+        }
+
+        private void GoNextForm_Click(object sender, EventArgs e)
+        {
+            DeliveredOrdersForm frm = new DeliveredOrdersForm();
+            this.Hide();
+            frm.Show();
+        }
+    }
+}
